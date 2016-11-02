667bff09fc8692b3d68751ec9a392bd977165cea
Olga <charminola@yahoo.com>
2016-11-02 18:13:48 +0200

    Update and rename promet_2_1 to promet_2_1.py

--- promet_2_1.py
+++ promet_2_1.py
@@ -6,4 +6,6 @@ a = float(sys.argv[1])
 b = float(sys.argv[2])
 c = float(sys.argv[3])
 print math.exp((((a-b)**2)/(2*(c**2)))*-1)/(c*math.sqrt(2*math.pi))
-# not checked yet
+# checked and accepted
+# automatically generated answer was
+# print (1/(a*math.sqrt(2*math.pi)))*math.exp(-(x-m)**2/(2*a**2))
